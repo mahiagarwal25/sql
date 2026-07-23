1841afd27ed3ba023a2f28baf62467b3288c3585
mahiagarwal25 <mahiagarwal767@gmail.com>
2026-07-23 23:28:11 +0530

    Update 15july-sql.sql

--- 15july-sql.sql
+++ 15july-sql.sql
@@ -3,7 +3,7 @@ use world;
 select continent, count(name) from country group by continent
 having count(name)>10;
 -- where can not be applied bcz of aggregate functions
--- having can aplly over here
+-- having can apply over here
 
 select continent, count(name) from country where continent like "a%"
 group by continent having count(name)>10;
